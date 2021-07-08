0f62a33dd1590bfa7176cf673a448b76170a2c8e
Isaac Peña <isaac@repson.org>
2021-07-08 19:17:45 +0200

    ex1.3 files

---
+++ ex1.2/main.go
@@ -0,0 +1,15 @@
+// ex1.2 Modify the echo program to print the index and
+// value of each of its arguments, one per line
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for i, arg := range os.Args {
+		fmt.Printf("%d: %s\n", i, arg)
+	}
+}
